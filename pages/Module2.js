a44186f917236245d68a374c000176e53beeac2c
935338 <54897272+935338@users.noreply.github.com>
2019-10-23 09:04:20 -0400

    tk

--- pages/Module2.js
+++ pages/Module2.js
@@ -3,42 +3,48 @@
 function triangle() {
 
 	var a1 = parseInt(prompt("Enter the frist angle"));
+	
 	if (a1>0 && a1<179) {
-	var a2 = parseInt(prompt("Enter the second angle"));
-	var a3 = 180 - (a1+a2);		
-	alert("The third angle is" + " " + a3);
+		var a2 = parseInt(prompt("Enter the second angle"));
+		var a3 = 180 - (a1+a2);		
+		alert("The third angle is" + " " + a3);
 	}
+	
 	else {
-		alert("you typed something wrong, so re-enter the first angle")
+		alert("you typed something wrong, so re-enter the first angle as an integer")
 		var a1 = parseInt(prompt("Enter the frist angle"));
 		var a2 = parseInt(prompt("Enter the second angle"));
 		var a3 = 180 - (a1+a2);		
 		alert("The third angle is" + " " + a3);
 
 	}
-
-
 }
 
 function year() {
 	var inputYear = parseInt(prompt("Enter the year"));
+
 	if(inputYear>0 ) {
 		var century = Math.ceil(inputYear/100);
 		alert("the century of" + " " + inputYear+ " " + "is" + " " + century);
 	}
+
 	else {
-		alert("please enter a realistic year");
+		alert("please enter a year as an integer");
 	 }
 }
 
 
 function tenfour() {
-	for (i=0;i<=100;i++) {
-		if (i%4==0){
-			console.log("four");
-		}
-		elseif {
 
+	for (let i=1;i<=100;i++) {
+		if(i%4==0 && i%10==0) {
+			console.log("tenfour");
+		}
+		else if (i%4==0){
+			console.log("Four");
+		}
+		else if (i%10==0) {
+			console.log("Ten");
 		}
 		else{
 			console.log(i);
